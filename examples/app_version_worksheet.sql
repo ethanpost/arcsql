23b8762b7a56df4ef7513d190e36be54779e2747
Ethan Post <post.ethan@gmail.com>
2020-05-22 13:30:08 -0500

    Update app_version_worksheet.sql

--- examples/app_version_worksheet.sql
+++ examples/app_version_worksheet.sql
@@ -16,10 +16,15 @@ exec arcsql.confirm_app_version('BAR');
 
 -- Start my .sql file...
 
-exec arcsql.set_app_version('X', 1.1);
+whenever sqlerror exit failure;
+
+exec arcsql.set_app_version('X', 2.0);
 
 begin 
    if arcsql.get_app_version('X') < 1.1 then
+      -- Do something...
+   end if;
+   if arcsql.get_new_version('X') = 2.0 and arcsql.get_app_version('X') = 1.9 then
       -- Do something...
    end if;
 end;
