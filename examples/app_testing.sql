f659d21b6471f71487234a2f4fa1cbb312e9100d
Ethan Post <post.ethan@gmail.com>
2020-06-05 15:16:09 -0500

    Update app_testing.sql

--- examples/app_testing.sql
+++ examples/app_testing.sql
@@ -1,7 +1,14 @@
 
-declare
+/* 
+-----------------------------------------------------------------------------------
+Application Profile Tests
+-----------------------------------------------------------------------------------
+*/
+
+create or replace procedure test_app_test_profiles is 
    n number;
 begin 
+   return;
    --
    arcsql.init_test('Use default profile when defined.');
    
@@ -122,7 +129,6 @@ begin
       arcsql.fail_test;
    end if;
 
-
    -- 
    arcsql.init_test('** Success** ');
    arcsql.pass_test;
@@ -133,98 +139,112 @@ exception
 end;
 /
 
--- declare 
---    n number;
--- begin 
---    -- Setup
---    delete from app_test where test_name='bar';
---    delete from app_test_profile where profile_name='foo';
-   
---    -- 
---    arcsql.init_test('Add a new app profile.');
---    -- This will not throw error if the app profile already exists.
---    arcsql.add_app_test_profile(p_profile_name=>'foo');
---    arcsql.g_app_test_profile.test_interval := 0;
---    arcsql.save_app_test_profile;
---    select 1 into n from app_test_profile where profile_name='foo' and env_type is null;
---    arcsql.pass_test;
-   
---    --
---    arcsql.init_test('Test does not exist.');
---    select 1 into n from app_test where test_name='bar' having count(*)=0;
---    arcsql.pass_test;
-   
---    --
---    arcsql.init_test('Init test raises error if profile not set.');
---    arcsql.g_app_test_profile := null;
---    begin 
---       if arcsql.init_app_test(p_test_name=>'bar') then
---          arcsql.fail_test;
---       end if;
---    exception 
---       when others then 
---          arcsql.pass_test;
---    end;
-   
---    -- 
---    arcsql.init_test('Set profile to foo even when env_type not found.');
---    arcsql.set_app_test_profile(p_profile_name=>'foo', p_env_type=>'x');
+/* 
+-----------------------------------------------------------------------------------
+Basic Features
+-----------------------------------------------------------------------------------
+*/
 
---    --
---    arcsql.init_test('Create a simple test.');
---    arcsql.set_app_test_profile(p_profile_name=>'foo');
---    if arcsql.init_app_test(p_test_name=>'bar') then
---       arcsql.pass_test;
---    else 
---       arcsql.fail_test;
---    end if;
+create or replace procedure test_basic_app_profile_features is 
+   n number;
+begin 
+   delete from app_test_profile where profile_name in ('foo', 'bar', 'default');
+   delete from app_test;
+   commit;
    
---    --
---    arcsql.init_test('App test expected to fail.');
---    if arcsql.init_app_test(p_test_name=>'bar') then
---       arcsql.app_test_fail;
---    end if;
---    arcsql.assert := arcsql.g_app_test.test_status = 'FAIL';
---    arcsql.test;
+   -- 
+   arcsql.init_test('Add a new app profile.');
+   -- This will not throw error if the app profile already exists.
+   arcsql.add_app_test_profile(p_profile_name=>'foo');
+   arcsql.g_app_test_profile.test_interval := 0;
+   arcsql.save_app_test_profile;
+   select 1 into n from app_test_profile where profile_name='foo' and env_type is null;
+   arcsql.pass_test;
    
---    --
---    arcsql.init_test('App test expected to pass.');
---    if arcsql.init_app_test(p_test_name=>'bar') then
---       arcsql.app_test_pass;
---    end if;
---    arcsql.assert := arcsql.g_app_test.test_status = 'PASS';
---    arcsql.test;
-
---    --
---    arcsql.init_test('App test expected to retry twice, then fail.');
---    arcsql.g_app_test_profile.retry_count := 2;
+   --
+   arcsql.init_test('Test does not exist.');
+   select 1 into n from app_test where test_name='bar' having count(*)=0;
+   arcsql.pass_test;
    
---    if arcsql.init_app_test(p_test_name=>'bar') then 
---       arcsql.g_app_test.test_status := 'PASS';
---       arcsql.app_test_fail;
---       arcsql.app_test_fail;
---       if arcsql.g_app_test.test_status != 'RETRY' then 
---          arcsql.fail_test;
---       end if;
---       arcsql.app_test_fail;
---       if arcsql.g_app_test.test_status != 'FAIL' then 
---          arcsql.fail_test;
---       end if;
---    end if;
---    arcsql.pass_test;
+   --
+   arcsql.init_test('Init test raises error if profile not set.');
+   arcsql.g_app_test_profile := null;
+   begin 
+      if arcsql.init_app_test(p_test_name=>'bar') then
+         arcsql.fail_test;
+      end if;
+   exception 
+      when others then 
+         arcsql.pass_test;
+   end;
+   
+   -- 
+   arcsql.init_test('Set profile to foo even when env_type not found.');
+   arcsql.set_app_test_profile(p_profile_name=>'foo', p_env_type=>'x');
 
---    --
---    arcsql.init_test('App test expected to pass again.');
---    if arcsql.init_app_test(p_test_name=>'bar') then
---       arcsql.app_test_pass;
---    end if;
---    arcsql.assert := arcsql.g_app_test.test_status = 'PASS';
---    arcsql.test;
+   --
+   arcsql.init_test('Create a simple test.');
+   arcsql.set_app_test_profile(p_profile_name=>'foo');
+   if arcsql.init_app_test(p_test_name=>'bar') then
+      arcsql.pass_test;
+   else 
+      arcsql.fail_test;
+   end if;
+   
+   --
+   arcsql.init_test('App test expected to fail.');
+   if arcsql.init_app_test(p_test_name=>'bar') then
+      arcsql.app_test_fail;
+   end if;
+   arcsql.assert := arcsql.g_app_test.test_status = 'FAIL';
+   arcsql.test;
+   
+   --
+   arcsql.init_test('App test expected to pass.');
+   if arcsql.init_app_test(p_test_name=>'bar') then
+      arcsql.app_test_pass;
+   end if;
+   arcsql.assert := arcsql.g_app_test.test_status = 'PASS';
+   arcsql.test;
 
--- exception
---    when others then
---       arcsql.fail_test;
--- end;
--- /
+   --
+   arcsql.init_test('App test expected to retry twice, then fail.');
+   arcsql.g_app_test_profile.retry_count := 2;
+   
+   if arcsql.init_app_test(p_test_name=>'bar') then 
+      arcsql.g_app_test.test_status := 'PASS';
+      arcsql.app_test_fail;
+      arcsql.app_test_fail;
+      if arcsql.g_app_test.test_status != 'RETRY' then 
+         arcsql.fail_test;
+      end if;
+      arcsql.app_test_fail;
+      if arcsql.g_app_test.test_status != 'FAIL' then 
+         arcsql.fail_test;
+      end if;
+   end if;
+   arcsql.pass_test;
+
+   --
+   arcsql.init_test('App test expected to pass again.');
+   if arcsql.init_app_test(p_test_name=>'bar') then
+      arcsql.app_test_pass;
+   end if;
+   arcsql.assert := arcsql.g_app_test.test_status = 'PASS';
+   arcsql.test;
+
+   -- 
+   arcsql.init_test('Basic Features Success');
+   arcsql.pass_test;
+
+exception
+   when others then
+      arcsql.fail_test;
+end;
+/
+
+
+-- exec test_app_test_profiles;
+exec test_basic_app_profile_features;
 
 
